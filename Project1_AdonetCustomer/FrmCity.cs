a9df02aac8ff73850e21876db8c8d3dde6e5d374
mehmetalbyrak <albayrakmehmetdeu@gmail.com>
2024-09-09 10:12:47 +0300

    Add project files.

---
+++ Project1_AdonetCustomer/FrmCity.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1_AdonetCustomer
+{
+    public partial class FrmCity : Form
+    {
+        public FrmCity()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        SqlConnection sqlConnection = new SqlConnection(@"Server=DESKTOP-LAM2LI6\SQLEXPRESS; Initial Catalog=CustomerDB; Integrated Security=true; TrustServerCertificate=True");
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand("Select * from TblCity", sqlConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+
+
+            sqlConnection.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand("insert into TblCity (CityName, CityCountry) values (@cityName, @cityCountry)", sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@cityName", txtCityName.Text);
+            sqlCommand.Parameters.AddWithValue("@cityCountry", txtCountry.Text);
+            sqlCommand.ExecuteNonQuery(); // affected row count --> etkilenen kayıt sayısı
+            sqlConnection.Close();
+
+            MessageBox.Show("Successfully added :d");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // delete process generally implemented according to id :d
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("delete from TblCity where CityId = @cityId", sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@cityId", txtCityId.Text);
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            MessageBox.Show("City is deleted as successfully", "Warn!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("Update TblCity Set CityName = @cityName, CityCountry = @cityCountry where CityId = @cityId", sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@cityName", txtCityName.Text);
+            sqlCommand.Parameters.AddWithValue("@cityCountry", txtCountry.Text);
+            sqlCommand.Parameters.AddWithValue("@cityId", txtCityId.Text);
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            MessageBox.Show("City is updated as successfully", "Warn!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("Select * from TblCity where CityName = @cityName", sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@cityName", txtCityName.Text);
+            SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+            sqlConnection.Close();
+
+
+        }
+    }
+}
---
+++ Project1_AdonetCustomer/FrmMap.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Project1_AdonetCustomer
+{
+    partial class FrmMap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCityOperations = new System.Windows.Forms.Button();
+            this.btnCustomerOperations = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCityOperations
+            // 
+            this.btnCityOperations.Location = new System.Drawing.Point(97, 59);
+            this.btnCityOperations.Name = "btnCityOperations";
+            this.btnCityOperations.Size = new System.Drawing.Size(187, 75);
+            this.btnCityOperations.TabIndex = 0;
+            this.btnCityOperations.Text = "City Operations";
+            this.btnCityOperations.UseVisualStyleBackColor = true;
+            this.btnCityOperations.Click += new System.EventHandler(this.btnCityOperations_Click);
+            // 
+            // btnCustomerOperations
+            // 
+            this.btnCustomerOperations.Location = new System.Drawing.Point(97, 189);
+            this.btnCustomerOperations.Name = "btnCustomerOperations";
+            this.btnCustomerOperations.Size = new System.Drawing.Size(187, 75);
+            this.btnCustomerOperations.TabIndex = 1;
+            this.btnCustomerOperations.Text = "Customer Operations";
+            this.btnCustomerOperations.UseVisualStyleBackColor = true;
+            this.btnCustomerOperations.Click += new System.EventHandler(this.btnCustomerOperations_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(97, 308);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(187, 75);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit Button";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FrmMap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 421);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCustomerOperations);
+            this.Controls.Add(this.btnCityOperations);
+            this.Name = "FrmMap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Forms";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCityOperations;
+        private System.Windows.Forms.Button btnCustomerOperations;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
