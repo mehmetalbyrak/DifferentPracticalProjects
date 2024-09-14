51495eeeb6addb65afdafc0e87bdd09f4fcc0349
mehmetalbyrak <albayrakmehmetdeu@gmail.com>
2024-09-14 18:31:22 +0300

    Added Project6_APIWeather

---
+++ Project6_APIWeather/Context/WeatherContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project6_APIWeather.Entities;
+
+namespace Project6_APIWeather.Context
+{
+    public class WeatherContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-LAM2LI6\\SQLEXPRESS; Initial Catalog=APIWeatherDB;\r\nIntegrated Security=true; TrustServerCertificate=True");
+        }
+
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ Project6_APIWeather/Controllers/Weathers.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project6_APIWeather.Context;
+using Project6_APIWeather.Entities;
+
+namespace Project6_APIWeather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Weathers : ControllerBase
+    {
+        WeatherContext weatherContext = new WeatherContext();
+
+        [HttpGet] // listing
+
+        public IActionResult WeatherCityList()
+        {
+            var values = weatherContext.Cities.ToList();
+            return Ok(values);
+        }
+
+
+        [HttpPost]
+        public IActionResult AddToCityForWeather(City city)
+        {
+            var value = weatherContext.Cities.Add(city);
+            weatherContext.SaveChanges();
+            return Ok("City added");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteToCityForWeather(int id)
+        {
+            var value = weatherContext.Cities.Find(id);
+            weatherContext.Cities.Remove(value);
+            weatherContext.SaveChanges();
+            return Ok("City deleted");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateToCityForWeather(City city)
+        {
+            var value = weatherContext.Cities.Find(city.CityId);
+            value.CityName = city.CityName;
+            value.Detail = city.Detail;
+            value.Temp = city.Temp;
+            value.Country = city.Country;
+            weatherContext.SaveChanges();
+            return Ok("City is updated");
+        }
+
+        [HttpGet("GetById")]
+
+        public IActionResult GetCityWeatherById(int id)
+        {
+            var value = weatherContext.Cities.Find(id);
+            return Ok(value);
+        }
+
+        [HttpGet("TotalCityCount")]
+
+        public IActionResult GetTotalCityCount()
+        {
+            var value = weatherContext.Cities.Count();
+            return Ok(value);
+        }
+
+        [HttpGet("HighestTempCityName")]
+
+        public IActionResult HighestTempCityName()
+        {
+            var value = weatherContext.Cities
+                .OrderByDescending(x=>x.Temp)
+                .Select(x=>x.CityName)
+                .FirstOrDefault();
+
+            return Ok(value);
+        }
+
+        [HttpGet("LowestTempCityName")]
+
+        public IActionResult LowestTempCityName()
+        {
+            var value = weatherContext.Cities
+                .OrderBy(x => x.Temp)
+                .Select(x => x.CityName)
+                .FirstOrDefault();
+
+            return Ok(value);
+        }
+
+
+
+
+    }
+}
