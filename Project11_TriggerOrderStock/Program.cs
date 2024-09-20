bef870eb4b791db7d8de01f3690901cca6d46dc7
mehmetalbyrak <albayrakmehmetdeu@gmail.com>
2024-09-21 02:12:25 +0300

    Project11_TriggerOrderStock Intro

---
+++ Project11_TriggerOrderStock/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project11_TriggerOrderStock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            Console.WriteLine("Stock Order System");
+            Console.WriteLine();
+            Console.WriteLine("1- Product Lists");
+            Console.WriteLine("2- Order Lists");
+            Console.WriteLine("3- Account State");
+            Console.WriteLine("4- New Product Sales");
+            Console.WriteLine("5- Product Stock Updating");
+            Console.WriteLine();
+            Console.WriteLine("----------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter the action you want :d ");
+            Console.ReadLine();
+
+            number = Console.ReadLine();
+
+            if (number != null)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
